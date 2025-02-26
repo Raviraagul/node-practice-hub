9f4ddd29846a78eb6dada9e04402d08cea97575a
RaviRaagul <raviraagul23@gmail.com>
2025-02-26 15:17:35 +0530

    Added code in books amd Updated other files

--- Book Directory/src/models/books.js
+++ Book Directory/src/models/books.js
@@ -1,14 +1,14 @@
-const express = require("express");
 const psql = require('../config/database');
-const { json } = require("body-parser");
 
 exports.bookList = function( req, res, next ){
     try{
         let sql = "Select * from books;"
+        // console.log("DEBUG: SQL Query =", sql);
+
         psql.query(sql, function( error, result ){
             if(error)
             {
-                res.json ({"error": error});
+                res.json ({"Query Error List: ": error});
             }
             else
             {
@@ -19,6 +19,56 @@ exports.bookList = function( req, res, next ){
         });
     } catch( error )
     {
-        res.json ({"error": error});
+        res.json ({"Catch Error: ": error});
     }
-}
+};
+
+exports.addBook = function( req, res, next ){
+    try{
+        let title = req.body.title;
+        let author = req.body.author;
+        let isbn = req.body.isbn;
+
+        const sql = "insert into books(title, author, isbn) values ('"+title+"', '"+author+"', '"+isbn+"') returning id;";
+
+        psql.query(sql, function( error, result ){
+            if(error)
+            {
+                res.json ({"Query Error Add: ": error});
+            }
+            else
+            {
+                let bookId = result.rows[0].id;
+                res.json ({"Message": "Added Successfully","ID": bookId});
+            }
+        });
+    } catch(error )
+    {
+        res.json ({"Catch Error Add": error});
+    }
+};
+
+exports.updateBook = function( req, res, next ){
+    try{
+        let id = req.body.bookId;
+        let title = req.body.title;
+        let author = req.body.author;
+        let isbn = req.body.isbn;
+
+        const sql = "update books set title = '"+title+"' and author = '"+author+"' and isbn = '"+isbn+"' where id = '"+id+"';";
+
+        psql.query(sql, function( error, result ){
+            if(error)
+            {
+                res.json ({"Query Error Update: ": error});
+            }
+            else
+            {
+                res.json ({"Message": "Updated Successfully","ID": bookId});
+            }
+        });
+    } catch(error )
+    {
+        res.json ({"Catch Error Update": error});
+    }
+};
--- Book Directory/src/server.js
+++ Book Directory/src/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 // const dotenv = require('dotenv');
 require('dotenv').config();
 const psql = require('./config/database');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
+
+// Middleware to parse JSON request bodies
+app.use(bodyParser.json()); 
 
 const routes = require('./routes');
 
@@ -31,4 +35,6 @@ app.use('/', routes);
     }
 }); */
 
-app.listen(port);
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
